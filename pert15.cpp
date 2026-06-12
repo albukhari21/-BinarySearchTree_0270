d0e4cda3f30273b7b74baa6da467e32fa321a3b5
albukhari21 <albukhariinsankamil@gmail.com>
2026-06-12 16:09:09 +0700

    Implement Node class for binary search tree with member variables

--- pert15.cpp
+++ pert15.cpp
@@ -1,2 +1,11 @@
 #include <iostream>
 using namespace std;
+
+// membuat class node
+class Node {
+
+    // memberi akses modifier serta mendeklarasi variabel dan object node
+public :
+    string info;
+    Node* leftChild;
+    Node* rightChild;
