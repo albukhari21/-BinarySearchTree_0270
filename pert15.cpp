22ba8393ed1243b31dafb902dd1a004461ab7d0f
albukhari21 <albukhariinsankamil@gmail.com>
2026-06-12 16:14:37 +0700

    Implement preorder traversal method in BinaryTree class

--- pert15.cpp
+++ pert15.cpp
@@ -92,3 +92,17 @@ public :
             inorder(ptr->rightChild);
         }
     }
+
+    // membuat dan mendefinisikan prosedur preorder
+    void preorder(Node* ptr) {
+
+        if(ROOT == nullptr) {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+        if (ptr != nullptr) {
+            cout << ptr->info << " ";
+            preorder(ptr->leftChild);
+            preorder(ptr->rightChild);
+        }
+    }
